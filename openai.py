ce5ce48b87b53edc496c2d384f28b9f020261d99
Søren Porskrog <porskrog@dalgashave.dk>
2023-10-15 20:13:58 +0200

    Automated commit

--- openai.py
+++ openai.py
@@ -1,12 +1,18 @@
+import os
 import openai
-import json
 
-openai.api_key = "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"
+# Access the API key from environment variable
+api_key = os.environ.get('OPENAI_API_KEY')
 
-response = openai.Completion.create(
-  engimodel="GPT-4",
-  prompt="Translate the following English text to French: '{}'",
-  max_tokens=60
-)
-
-print(json.dumps(response, indent=4))
+# Example API call to GPT-4
+try:
+    response = openai.Completion.create(
+        engine="GPT-4",  # Replace with GPT-4 engine ID when available
+        prompt="your-prompt-here",
+        max_tokens=100,
+        api_key=api_key  # Pass the API key here
+    )
+    generated_text = response.choices[0].text.strip()
+    print(f"Generated text: {generated_text}")
+except Exception as e:
+    print(f"An error occurred: {e}")
