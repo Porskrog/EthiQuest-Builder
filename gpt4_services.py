e5f7ab08f7596f476df39b1c5a5f58e3e98d87e8
Porskrog <100382927+Porskrog@users.noreply.github.com>
2023-10-30 22:42:53 +0100

    Automated commit

--- gpt4_services.py
+++ gpt4_services.py
@@ -99,15 +99,15 @@ def generate_new_dilemma_with_gpt4(last_dilemma=None, last_option=None, user_id=
     
     # Decide whether to make the new dilemma consequential of the former dilemma
     # make_consequential = choice([True, False])
-    make_consequential = choice([True, True])
+    make_consequential = choice([True, True, False])
 
     # Generate the prompt for GPT-4
     base_prompt = "Please generate an ethical and leadership dilemma related to program management."
     if make_consequential and last_dilemma and last_option:
-        full_prompt = f"{base_prompt} It must be a direct consequence of the previous dilemma was: {last_dilemma} and the chosen option which was: {last_option}"
+        full_prompt = f"{base_prompt} It must be a direct consequence of the previous dilemma which was: {last_dilemma.question} and the chosen option which was: {last_option.text}"
     else:
         full_prompt = base_prompt
-
+    
     # Add the standard format to the prompt
     full_prompt += """
     Format the dilemma as follows:
