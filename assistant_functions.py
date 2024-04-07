4b12bee7aebc6dbec318ed290b1bca0a1f1aad5a
Porskrog <100382927+Porskrog@users.noreply.github.com>
2024-04-07 10:56:49 +0200

    Automated commit

--- assistant_functions.py
+++
@@ -1,6 +0,0 @@
-# This is the main file that contains the functions that your Assistant would use to interact with external systems or databases.
-# You can define all the functions that your Assistant would need to interact with external systems or databases here.
-# This file would be imported in the main Assistant file (assistant.py) and the functions would be called from there.
-
-# Importing required libraries
-from flask import Flask
